208cf7de0255df97c5377206de07ec3898ebef44
Marcus Weiner <marcus.weiner@gmail.com>
2024-03-23 19:59:25 +0100

    Create VLAN interfaces

--- modules/device/interfaces.tf
+++ modules/device/interfaces.tf
@@ -9,3 +9,27 @@ data "netbox_device_interfaces" "lo" {
     value = "lo"
   }
 }
+
+data "netbox_device_interfaces" "eth1" {
+  filter {
+    name  = "device_id"
+    value = var.device_id
+  }
+
+  filter {
+    name  = "name"
+    value = "eth1"
+  }
+}
+
+resource "netbox_device_interface" "lan" {
+  for_each = netbox_vlan.networks
+
+  device_id = var.device_id
+  name      = "${one(data.netbox_device_interfaces.eth1.interfaces).name}.${each.value.vid}"
+  type      = "virtual"
+
+  parent_device_interface_id = one(data.netbox_device_interfaces.eth1.interfaces).id
+
+  description = each.value.name
+}
