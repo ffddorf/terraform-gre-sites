c1fca66cde430c7412bbe40fbeca280132885f16
Marcus Weiner <marcus.weiner@gmail.com>
2024-04-01 23:04:38 +0200

    Import R10

---
+++ import-r10.tf
@@ -0,0 +1,148 @@
+import {
+  to = module.device["R10"].netbox_available_prefix.site_v4
+  id = "96 381 32"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.site_v6
+  id = "97 135 56"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v4["CR3"]
+  id = "126 147 31"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v4["CR4"]
+  id = "126 148 31"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v4["CR5"]
+  id = "126 167 31"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v6["CR3"]
+  id = "132 142 64"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v6["CR4"]
+  id = "132 143 64"
+}
+import {
+  to = module.device["R10"].netbox_available_prefix.tunnels_v6["CR5"]
+  id = "132 164 64"
+}
+import {
+  to = module.device["R10"].netbox_device_interface.tunnels["CR3"]
+  id = "746"
+}
+import {
+  to = module.device["R10"].netbox_device_interface.tunnels["CR4"]
+  id = "941"
+}
+import {
+  to = module.device["R10"].netbox_device_interface.tunnels["CR5"]
+  id = "1190"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v4["CR3"]
+  id = "281"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v4["CR4"]
+  id = "301"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v4["CR5"]
+  id = "350"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v6["CR3"]
+  id = "290"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v6["CR4"]
+  id = "303"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.local_tunnel_address_v6["CR5"]
+  id = "352"
+}
+import {
+  to = module.device["R10"].netbox_prefix.loopback_v6
+  id = "136"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.loopback_v4
+  id = "278"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.loopback_v6
+  id = "285"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v4["CR3"]
+  id = "280"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v4["CR4"]
+  id = "298"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v4["CR5"]
+  id = "358"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v6["CR3"]
+  id = "289"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v6["CR4"]
+  id = "302"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.remote_tunnel_address_v6["CR5"]
+  id = "355"
+}
+import {
+  to = module.device["R10"].netbox_prefix.local_v4
+  id = "139"
+}
+import {
+  to = module.device["R10"].netbox_prefix.networks_v4["1"]
+  id = "140"
+}
+import {
+  to = module.device["R10"].netbox_prefix.networks_v4["2"]
+  id = "141"
+}
+import {
+  to = module.device["R10"].netbox_prefix.networks_v6["1"]
+  id = "137"
+}
+import {
+  to = module.device["R10"].netbox_prefix.networks_v6["2"]
+  id = "138"
+}
+import {
+  to = module.device["R10"].netbox_vlan.networks["1"]
+  id = "50"
+}
+import {
+  to = module.device["R10"].netbox_vlan.networks["2"]
+  id = "52"
+}
+import {
+  to = module.device["R10"].netbox_vlan_group.group
+  id = "9"
+}
+import {
+  to = module.device["R10"].netbox_vrf.local
+  id = "3"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.router_addresses_v4["1"]
+  id = "295"
+}
+import {
+  to = module.device["R10"].netbox_ip_address.router_addresses_v4["2"]
+  id = "296"
+}
