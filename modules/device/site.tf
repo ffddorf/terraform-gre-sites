47288b53f33baa16046d70d5573087042badd06c
Marcus Weiner <marcus.weiner@gmail.com>
2024-07-06 16:14:09 +0200

    Add site names to resource descriptions

--- modules/device/site.tf
+++ modules/device/site.tf
@@ -1,9 +1,17 @@
+data "netbox_site" "current" {
+  id = var.site_id
+}
+
+locals {
+  location = "${var.name} (${data.netbox_site.current.name})"
+}
+
 resource "netbox_available_prefix" "site_v4" {
   parent_prefix_id = var.sites_prefix_v4_id
   prefix_length    = 32
 
   status      = "active"
-  description = "Site prefix for ${var.name}"
+  description = "Site prefix for ${local.location}"
   tenant_id   = var.tenant_id
   site_id     = var.site_id
 }
@@ -11,7 +19,7 @@ resource "netbox_available_prefix" "site_v4" {
 resource "netbox_ip_address" "loopback_v4" {
   ip_address  = "${cidrhost(netbox_available_prefix.site_v4.prefix, 0)}/${netbox_available_prefix.site_v4.prefix_length}"
   status      = "active"
-  description = "Loopback address for ${var.name}"
+  description = "Loopback address for ${local.location}"
 
   device_interface_id = one(data.netbox_device_interfaces.lo.interfaces).id
   tenant_id           = var.tenant_id
@@ -28,7 +36,7 @@ resource "netbox_available_prefix" "site_v6" {
   prefix_length    = 56
 
   status      = "active"
-  description = "Site prefix for ${var.name}"
+  description = "Site prefix for ${local.location}"
   tenant_id   = var.tenant_id
   site_id     = var.site_id
 }
@@ -41,7 +49,7 @@ locals {
 resource "netbox_prefix" "loopback_v6" {
   prefix      = cidrsubnet(netbox_available_prefix.site_v6.prefix, local.loopback_prefix_newbits, 0)
   status      = "active"
-  description = "Loopback prefix for ${var.name}"
+  description = "Loopback prefix for ${local.location}"
   tenant_id   = var.tenant_id
   site_id     = var.site_id
 }
@@ -49,7 +57,7 @@ resource "netbox_prefix" "loopback_v6" {
 resource "netbox_ip_address" "loopback_v6" {
   ip_address  = "${cidrhost(netbox_prefix.loopback_v6.prefix, 1)}/${local.loopback_prefix_length}"
   status      = "active"
-  description = "Loopback address for ${var.name}"
+  description = "Loopback address for ${local.location}"
 
   device_interface_id = one(data.netbox_device_interfaces.lo.interfaces).id
   tenant_id           = var.tenant_id
