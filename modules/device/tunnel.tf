52241290ac42046ee34e191d18f5563e012da21d
Marcus Weiner <marcus.weiner@gmail.com>
2024-07-08 00:52:30 +0200

    Revert "Set outside IP on site tunnel terminations (#19)"
    This reverts commit 01fe39935cac03f6bc63ae680d3f7229174f40d3.

--- modules/device/tunnel.tf
+++ modules/device/tunnel.tf
@@ -87,6 +87,5 @@ resource "netbox_vpn_tunnel_termination" "site" {
   tunnel_id = each.value.id
   role      = "peer"
 
-  device_interface_id   = netbox_device_interface.tunnels[each.key].id
-  outside_ip_address_id = netbox_ip_address.gre_dnat.id
+  device_interface_id = netbox_device_interface.tunnels[each.key].id
 }
