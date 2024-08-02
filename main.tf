6bf5ca5a12715522b87f80a5b89fc43d2f84bea4
Marcus Weiner <marcus.weiner@gmail.com>
2024-08-03 00:36:56 +0200

    Manage core router tunnel interfaces (#23)
    Applied locally 🙈

--- main.tf
+++ main.tf
@@ -2,10 +2,33 @@ locals {
   devices = {
     for dev in data.netbox_devices.devices.devices : dev.name => dev
   }
+  core_devices = concat(
+    [for dev in data.netbox_devices.core_routers.devices : {
+      id     = dev.device_id
+      name   = dev.name
+      device = dev
+    }],
+    [for vm in data.netbox_virtual_machines.core_routers.vms : {
+      id   = vm.vm_id
+      name = vm.name
+      vm   = vm
+    }],
+  )
 }
 
 resource "netbox_vpn_tunnel_group" "sites" {
   name = "site-tunnels"
+}
+
+module "tunnel_interfaces" {
+  for_each = { for dev in local.core_devices : dev.name => dev }
+
+  source = "./modules/available_interfaces"
+
+  prefix      = "tun"
+  device_id   = each.value.id
+  device_type = can(each.value.vm) ? "vm" : "device"
+  targets     = [for name, dev in local.devices : name]
 }
 
 module "device" {
@@ -24,10 +47,12 @@ module "device" {
   tunnel_prefix_v4_id = data.netbox_prefix.tunnels_prefix_v4.id
   tunnel_prefix_v6_id = data.netbox_prefix.tunnels_prefix_v6.id
 
-  tunnel_peer_names = concat(
-    [for dev in data.netbox_devices.core_routers.devices : dev.name],
-    [for vm in data.netbox_virtual_machines.core_routers.vms : vm.name],
-  )
+  core_tunnels = [for dev in local.core_devices : {
+    name        = dev.name
+    device_id   = dev.id
+    device_type = can(dev.vm) ? "vm" : "device"
+    if_name     = module.tunnel_interfaces[dev.name].interface_names[each.key]
+  }]
 
   tunnel_prefix_role_id = data.netbox_ipam_role.transfer.id
 
